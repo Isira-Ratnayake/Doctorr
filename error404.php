43636e9f8c733d8ed0e92b971f6c54d0c8766114
janiththilanka <70831439+janiththilanka@users.noreply.github.com>
2021-07-07 23:51:39 +0530

    Add files via upload

---
+++ error404.php
@@ -0,0 +1,48 @@
+<?php
+    $error_message = filter_input(INPUT_GET,'errorMsg');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Janith Thilanka">
+        <title>404-Error: Page Not Found</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> <!-- Adds bootstrap CSS to webpage -->
+        <script src="js/bootstrap.min.js"></script> <!-- Add Bootstrap scripts to page -->
+        <link rel="stylesheet" type="text/css" href="About_Us.css"> <!-- Links custom CSS style sheets to webpage -->
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row align-items-center workspace">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <div class="row px-5">
+                                <div class="col">
+                                    <h1 class="display-1">404</h1>
+                                </div>
+                            </div>
+                            <div class="row px-5">
+                                <div class="col">
+                                    <p class="text-justify lead">
+                                        <strong>An error occured while we were accessing your content:</strong><br>
+                                        <?php 
+                                            echo $error_message;
+                                        ?>
+                                    </p>
+                                    <div class="lead">
+                                        <small><a href="index.html" class="btn btn-primary">Back&nbsp;to&nbsp;Home</a></small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col col-lg-5 col-12 text-center">
+                            <img src="images/errorsteth.jpg" class="img-fluid steth-logo">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
